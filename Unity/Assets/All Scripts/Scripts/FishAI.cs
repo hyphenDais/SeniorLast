6e8afab0afeae5e159a5ec3337c2fbf1d0b92530
Unknown <nderey6@gmail.com>
2018-04-11 15:26:03 -0600

    Added Script Folder

---
+++ Unity/Assets/All Scripts/Scripts/FishAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAI : MonoBehaviour 
+{
+	public Transform target;
+	public Transform currentPosition;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward*5*Time.deltaTime);
+	}
+}
